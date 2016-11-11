f50123c9295ceb72fc4327f4ff0b49a0d1ff10ab
Sherry Yang <yangxiaoli1994@gmail.com>
2016-11-11 11:05:06 +0100

    updated 5

--- Computer Vision/exercise-02/q5_image_derivatives/gaussderiv.m
+++ Computer Vision/exercise-02/q5_image_derivatives/gaussderiv.m
@@ -1,8 +1,7 @@
 function [imgDx, imgDy] = gaussderiv(img, sigma)
 % insert your code here
-    derivative_x = gaussdx(-1:1, sigma);
+    derivative_x = gaussdx(-3*sigma:3*sigma, sigma);
     imgDx = conv2(img, derivative_x, 'same');
-    derivative_y = gaussdx(-1:1, sigma);
+    derivative_y = gaussdx(-3*sigma:3*sigma, sigma);
     imgDy = conv2(img, derivative_y', 'same');    
-
 end
