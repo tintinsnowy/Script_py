cfe017175398546e48c323ec98eb5dedc09000f2
Sherry Yang <yangxiaoli1994@gmail.com>
2016-11-13 20:48:11 +0100

    add more comments

--- Computer Vision/exercise-02/q6_edge_detection/answers.m
+++ Computer Vision/exercise-02/q6_edge_detection/answers.m
@@ -1,5 +1,5 @@
 function a = answers()
-	a.difficulties_sigma_theta = 'What difficulties do you observe when choosing sigma and theta?';
-	a.nms = 'Describe your results.';
-	a.hysteresis = 'What do you observe?';
+	a.difficulties_sigma_theta = 'The bigger simage is, the fuzzier the picture is.the bigger theta is, the less information there is.';
+	a.nms = 'getadges have more clear details and than getadges2';
+	a.hysteresis = 'my canny has more noise';
 end
