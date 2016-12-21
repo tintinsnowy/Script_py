722db591febe28af0746d6552ba69f7a6bbb3b29
Sherry Yang <yangxiaoli1994@gmail.com>
2016-12-21 23:32:24 +0100

    finished exercise 2

--- Computer Vision/exercise-03/q2_mean_shift_clustering/meanshift.m
+++ Computer Vision/exercise-03/q2_mean_shift_clustering/meanshift.m
@@ -1,6 +1,19 @@
 function [labels, peaks] = meanshift(data, r)
+   [n,p]=size(data);
+   labels = zeros(1,p);
+   peaks = zeros(n,p);
+   thres = r/2;
+   
+   for i=1:p
+      current = findpeak(data,i,r);
+      labels(i) = i;
+	  peaks(:,i) = current;
+      %find the similar ones
+      for j=1:(i-1)
+			if sqrt(sum((current - peaks(:, j)).^2)) < thres
+				labels(i) = labels(j);
+			end
+		end
 
-
-	% insert your code here
-
+   end
 end
--- Computer Vision/exercise-03/q2_mean_shift_clustering/meanshift_opt.m
+++ Computer Vision/exercise-03/q2_mean_shift_clustering/meanshift_opt.m
@@ -1,6 +1,10 @@
 function [labels, peaks] = meanshift_opt(data, r)
-
-
-	% insert your code here
+   [n,p]=size(data);
+   labes = zeros(1,p);
+   peaks = zeros(n,p);
+   for i=1:p
+      current = findpeak(datam,data[:,i],r);
+      
+   end
 
 end
