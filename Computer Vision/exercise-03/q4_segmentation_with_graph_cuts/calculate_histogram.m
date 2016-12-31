fba9b6e2d79f77af1ed3983df6a5488e6897a13a
Sherry Yang <yangxiaoli1994@gmail.com>
2016-12-31 15:11:41 +0100

    finished the q4

--- Computer Vision/exercise-03/q4_segmentation_with_graph_cuts/calculate_histogram.m
+++ Computer Vision/exercise-03/q4_segmentation_with_graph_cuts/calculate_histogram.m
@@ -1,6 +1,19 @@
 function histogram = calculate_histogram(img, mask, n_bins)
 
+    [row, col, ~]=size(img);
+    histogram = 0.001 * ones(n_bins, n_bins, n_bins);
+    bins = min(n_bins, round( img/256 * n_bins ) + 1);% avoid the 0 and minus
+	 			
+	for y = 1:row
+		for x = 1:col
+			if mask(y,x) ~= 0
+				histogram( bins(y,x,1), bins(y,x,2), bins(y,x,3) ) = ...
+					histogram( bins(y,x,1), bins(y,x,2), bins(y,x,3) ) + 1;
+			end
+		end
+	end
 
-	% insert your code here
+	%normalize
+	histogram = histogram/sum(histogram(:));
 
 end
