2fdd781ef7ee2d6c0a9e7b31cd8db4f5a5eacec2
Sherry Yang <yangxiaoli1994@gmail.com>
2017-01-04 09:24:27 +0100

    finished the q2

--- Computer Vision/exercise-05/q2_matching/findnn.m
+++ Computer Vision/exercise-05/q2_matching/findnn.m
@@ -1,16 +1,16 @@
 function [Idx, Dist] = findnn(D1, D2)
 
 	% D1 D2: one histogram per line
-
+    [row,col]=size(D1);
 	% Idx should be a  vector,​where Idx(a) contains the index of the 
-
+    Idx = zeros(1,row);
 	% descriptor in D2 that matches the descriptor with index a in D1.
-
+    Dist = zeros(row,1);
 	% Dist should be a column vector that contains the distance between 
-
-	% those two descriptors.
-
-
-	% insert your code here
-
+    
+    for i = 1:row
+        E_dist =sum((D2-D1(i,:)).^2,2);
+       Dist(i) = min(E_dist);
+       Idx(i) =find(E_dist==Dist(i));
+    end 
 end
