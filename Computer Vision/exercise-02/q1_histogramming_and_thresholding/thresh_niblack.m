750c39b6e100cda666753042f3e79b99f65bb9d0
Sherry Yang <yangxiaoli1994@gmail.com>
2017-02-19 09:16:45 +0100

    the usage should be the square root of thevariance
    variance that we use is sigma^2

--- Computer Vision/exercise-02/q1_histogramming_and_thresholding/thresh_niblack.m
+++ Computer Vision/exercise-02/q1_histogramming_and_thresholding/thresh_niblack.m
@@ -5,7 +5,7 @@ res=img;
  for i=1:row
      for j=1:col
          [m,v]= get_window_mean_var(img,i,j,r);
-         T = m+k*v;
+         T = m+k*sqrt(v);
          res(max(1,i-r):min(i+r,row), max(1,j-r):min(j+r,col))= ...
          img(max(1,i-r):min(i+r,row), max(1,j-r):min(j+r,col))>=T;
      end
